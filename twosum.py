72d7ad7d3b23b86b2628b7e836a2d1fd858bdf6e
23f3000434 <ashuathu93@gmail.com>
2025-10-26 22:33:00 +0530

    twosum.py

---
+++ twosum.py
@@ -0,0 +1,35 @@
+# # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen_numbers = {} # This will store {number: index}
+  
+        # We will use enumerate to get both the index and value
+        for i, num in enumerate(nums):
+        # Calculate the complement
+            complement = target - num
+            # Your logic goes here.
+            if complement in seen_numbers:
+                 return(seen_numbers[complement], i)
+            else:
+                seen_numbers[num] = i
